116861faf9f0e88a0edf45827feeee9f73e6435e
Maheshilkaru <maheshilkarunarathna@gmail.com>
2025-04-25 15:35:58 +0530

    Add AggregatorServiceApplication.java and updated pom.xml

--- src/main/java/org/iit/cc/aggregator/AggregatorServiceApplication.java
+++ src/main/java/org/iit/cc/aggregator/AggregatorServiceApplication.java
@@ -1,1 +1,11 @@
 package org.iit.cc.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorServiceApplication.class, args);
+    }
+}
